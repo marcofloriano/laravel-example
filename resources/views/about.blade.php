a8c885877e112f41816a4accbf89fe596c079fdc
Marco Floriano <marcofloriano@gmail.com>
2024-10-08 14:10:38 -0300

    First commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>About Page</x-slot:heading>
+    <p>Welcome to the about page!</p>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Home Page</x-slot:heading>
+    <p>Welcome to the home page!</p>
+</x-layout>
